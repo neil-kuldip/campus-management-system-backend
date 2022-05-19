433d07df442870d2407acf9186713e024e7d67a6
Neil Kuldip <56613674+neil-kuldip@users.noreply.github.com>
2022-05-19 12:54:12 -0400

    fix: reset campus.js file

--- database/models/Campus.js
+++ database/models/Campus.js
@@ -1,24 +1,26 @@
 /*==================================================
-/database/db.js
-
-It sets up Sequelize with Postgres database. 
-- Create a Sequelize instance to connect to the database specifying database name, username, and password.
+/database/database/models/Campus.js
+It defines the campus model for the database.
 ==================================================*/
-/* INSTANTIATE DATABASE */ 
-
-// Import module dependencies
 const Sequelize = require('sequelize');  // Import Sequelize
-const {dbName, dbUser, dbPwd} = require('./utils/configDB');  // Import database name, username, password
+const db = require('../db');  // Import Sequelize database instance called "db"
 
-// Display a confirmation message for opening a database connection
-console.log('Opening database connection');
+// Define the campus model
+const Campus = db.define("campus", {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
 
-// This is the Sequelize entry point for connecting to the database. 
-// Instantiate the Sequelize instance with database name, username, and password. Then connect to the database.
-const db = new Sequelize(dbName, dbUser, dbPwd, {
-  host: 'localhost',
-  dialect: 'postgres'
+  address: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+
+  description: {
+    type: Sequelize.STRING,
+  }
 });
 
-// Export Sequelize instance, which will be modified with models.
-module.exports = db;
+// Export the campus model
+module.exports = Campus;
